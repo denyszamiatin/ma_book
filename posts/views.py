7d6e32d091fe7f5ff4fabcac2f4f195d13210a6b
koreyo <KoretsValentyn@gmail.com>
2020-01-16 01:06:39 +0200

    add main func create post

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 
-# Create your views here.
+
+def create_post(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:create_post')
+    return render(request, 'create_post.html', {'form': form})
+
