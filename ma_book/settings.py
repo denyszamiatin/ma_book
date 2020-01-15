e17d63ba267bbd78a3efaad313fd3a258a10afcc
Andrii Tomalia <logotip123@yahoo.com>
2020-01-15 17:31:46 +0200

    rebase base.html

--- ma_book/settings.py
+++ ma_book/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'ma_book.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
