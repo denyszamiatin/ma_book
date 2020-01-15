f5051fdc4dd8ed2dff05326066127665716f1cfa
Andrii Tomalia <logotip123@yahoo.com>
2020-01-15 14:50:40 +0200

    add user forms and html files

--- users/views.py
+++ users/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 
 def user_registration(request):
-    return render(request, 'users/registration.html')
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            print(form)
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password1'],
+                                email=form.cleaned_data['email'],
+                                first_name=form.cleaned_data['first_name'],
+                                last_name=form.cleaned_data['last_name'],)
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'users/registration.html', {
+        "form": form
+    })
