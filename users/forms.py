6bfc03f8b2982abfb007098f8ec2e759ad8119b1
Andrii Tomalia <logotip123@yahoo.com>
2020-01-15 15:21:15 +0200

    add user registration with bootstrap

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,24 @@ from django.contrib.auth.models import User
 
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=150)
-    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                              'placeholder': 'John'
+                                                                              }))
+    last_name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                              'placeholder': 'Smith'
+                                                                              }))
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                             'placeholder': 'your unique login'
+                                                                             }))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control',
+                                                                           'placeholder': 'example@mail.com'
+                                                                           }))
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                 'placeholder': 'password'
+                                                                                 }))
+    password2 = forms.CharField(label="Password confirm",widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                 'placeholder': 'confirm password'
+                                                                                 }))
 
     class Meta:
         model = User
