5e32e07dc1ff401f66012b43d939da05ae27a531
Andrii Tomalia <logotip123@yahoo.com>
2020-01-16 15:18:49 +0200

    adding profile __str__

--- users/models.py
+++ users/models.py
@@ -10,8 +10,12 @@ def content_file_name(instance, filename):
     filename = f"{instance.user.username}.{ext}"
     return Path(MEDIA_ROOT, "avatars", filename)
 
+
 class UserProfile(models.Model):
     birthday = models.DateField("Birthday")
     avatar = models.ImageField(upload_to=content_file_name, null=True, blank=True)
     status = models.CharField(max_length=MAX_STATUS_LENGTH, null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.user.username
