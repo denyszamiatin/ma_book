3c4387357e2f4d925cc08b3f6fce8e4fa783ebda
ValentinaLV <tina.lysenok@gmail.com>
2020-01-16 17:54:55 +0200

    added follower model

--- users/models.py
+++ users/models.py
@@ -10,8 +10,26 @@ def content_file_name(instance, filename):
     filename = f"{instance.user.username}.{ext}"
     return Path(MEDIA_ROOT, "avatars", filename)
 
+
 class UserProfile(models.Model):
     birthday = models.DateField("Birthday")
     avatar = models.ImageField(upload_to=content_file_name, null=True, blank=True)
     status = models.CharField(max_length=MAX_STATUS_LENGTH, null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+
+class Follower(models.Model):
+    user_from = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    user_to = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return "{} follows {}".format(self.user_from, self.user_to)
+
+
+UserProfile.add_to_class('following', models.ManyToManyField(
+    'self', through=Follower, related_name='followers', symmetrical=False
+))
