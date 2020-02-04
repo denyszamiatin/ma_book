d0df8f8c6e36277f680cc6b16705b07d899b2623
Senku1 <morhaliuk@gmail.com>
2020-02-04 15:59:41 +0200

    following_update

--- users/models.py
+++ users/models.py
@@ -28,3 +28,37 @@ class Relations(models.Model):
 
     def __repr__(self):
         return f"{self.current_user} follows {self.follows}"
+
+    @classmethod
+    def get_followers(cls, username, number=-1):
+        """
+        Return list of followers of a user. If parameter number is not set or negative, return all followers.
+        :param username: str  --  username of a user who's follower to return
+        :param number: int  --  number of followers to return
+        :return: list
+        """
+        if not isinstance(number, int):
+            raise TypeError('Integer value expected')
+        user = User.objects.get(username=username)
+        if number > 0:
+            followers = [relation.current_user for relation in cls.objects.filter(follows=user)][:number]
+        else:
+            followers = [relation.current_user for relation in cls.objects.filter(follows=user)]
+        return followers
+
+    @classmethod
+    def get_who_i_follow(cls, username, number=-1):
+        """
+        Return list of all people who user follows
+        :param username: str  --  username of user who follows
+        :param number: int  -- number of people who user follows to return
+        :return: list
+        """
+        if not isinstance(number, int):
+            raise TypeError('Integer value expected')
+        user = User.objects.get(username=username)
+        if number > 0:
+            people_i_follow = [relation.follows for relation in cls.objects.filter(current_user=user)][:number]
+        else:
+            people_i_follow = [relation.follows for relation in cls.objects.filter(current_user=user)]
+        return people_i_follow
